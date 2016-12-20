280965e0fb4542e822fe91ef26c161426fcc4956
ashokslsk <ashokslsk@gmail.com>
2016-12-20 14:57:54 +0530

    List view for the messages

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,13 +1,22 @@
 export class MainController {
-  constructor () {
+  constructor ($http) {
     'ngInject';
 
-
+      this.$http = $http;
+      this.getMessages();
   }
 
+    getMessages(){
+            var vm = this;
+            this.$http.get('http://localhost:5000/api/message').then(function(result){
+
+                vm.messages = result.data;
+
+            });
+
+    }
+
     postMessage(){
-
-        console.log("Clicked");
-
+        this.$http.post('http://localhost:5000/api/message', {msg: this.message});
     }
 }
